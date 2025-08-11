349ebf9e9a233e85783b2b2b8db4dde987b54ee0
Alessio Deriu <126460091+AlessioSalem@users.noreply.github.com>
2025-08-11 19:52:49 +0200

    Create analisi_porti.py
    ANALISI CREW CHANGE NEI PORTI IN VARI PAESI

---
+++ analisi_porti.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+# Dati originali
+dati = {
+    'Paese': ['SAINT LUCIA', 'CONGO', 'REUNION', 'NIGERIA', 'INDONESIA'],
+    'Scali': [5, 3, 2, 4, 7],
+    'Crew_Change': [None, None, None, None, None],
+    'Porti': [
+        'Castries, Vieux Fort',
+        'Boma, Matadi',
+        'Point des Galets, Reunion',
+        'Apapa, Lagos',
+        'Bahudopi, Balikpapan, Batam, Belawan, Jakarta, Palembang, Panjang, Surabaya'
+    ]
+}
+
+# Creazione DataFrame
+df = pd.DataFrame(dati)
+
+# Posiz. riga "SAINT LUCIA"
+try:
+    idx_saint_lucia = df[df['Paese'] == 'SAINT LUCIA'].index[0]
+    porto_saint_lucia = df.loc[idx_saint_lucia, 'Porti']
+    scali_saint_lucia = df.loc[idx_saint_lucia, 'Scali']
+
+    print("Posizione di SAINT LUCIA:", idx_saint_lucia)
+    print("Porto corrispondente:", porto_saint_lucia)
+    print("Numero di Scali:", scali_saint_lucia)
+except IndexError:
+    print("SAINT LUCIA non trovato nel DataFrame")
